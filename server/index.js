7c4bc04ae7803d09d50ef1a21037eba039c0fab7
Andreu Ramos <andreu.ramos.amengual@gmail.com>
2023-01-05 00:06:38 +0100

    start playing with api

--- server/index.js
+++ server/index.js
@@ -1,7 +1,25 @@
-import { Configuration, OpenAIApi } from "openai";
+const { Configuration, OpenAIApi } = require("openai");
 const configuration = new Configuration({
     organization: "org-noAd6Hu5vicXT3HSBXwaL6ks",
     apiKey: process.env.OPENAI_API_KEY,
 });
 const openai = new OpenAIApi(configuration);
-const response = await openai.listEngines();
+// const response = await openai.listEngines();
+
+async function callApi() {
+    const response = await openai.createCompletion({
+        "model": "text-davinci-003",
+        "prompt": "Say this is a test",
+        "max_tokens": 7,
+        "temperature": 0,
+        "top_p": 1,
+        "n": 1,
+        "stream": false,
+        "logprobs": null,
+        "stop": "\n"
+      });
+
+      console.log(response.data.choices[0].text)
+}
+
+callApi();
